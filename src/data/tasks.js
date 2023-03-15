bbcfd7451e08ada6e9f48fe7e6b867c1c38cdd82
adelson <tricker_bboy23@hotmail.com>
2023-03-14 20:04:05 -0400

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 1,
+        title: 'Do the dishes',
+        description: 'Do the dishes'
+    },
+    {
+        id: 2,
+        title: 'Do the laundry',
+        description: 'Do the laundry'
+    },
+    {
+        id: 3,
+        title: 'Do the homework',
+        description: 'Do the homework'
+    }
+];
